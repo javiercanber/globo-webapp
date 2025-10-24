cc4fba164893cb61bb016053c11f54854e6f2112
Javier <javier.candela.berrocal@gmail.com>
2025-10-24 09:52:52 +0200

    Switch to using Ansible for nginx install

--- variables.tf
+++ variables.tf
@@ -37,6 +37,11 @@ variable "api_key" {
   description = "(Required) API key for web app to talk to SaaS platform."
 }
 
+variable "playbook_repository" {
+  type        = string
+  description = "(Required) URL of the Ansible playbook repository."
+}
+
 # TFE Variables
 variable "tfe_organization" {
   type        = string
